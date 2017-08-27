06ca38be54d56d669958615d35b606e7a30661d9
nico <nico@localhost.localdomain>
2017-08-26 23:59:46 -0300

    Agregado test de permutacion

--- tp1/tp1.py
+++ tp1/tp1.py
@@ -7,7 +7,47 @@ import seaborn
 data0 = np.loadtxt('tiempos.txt', skiprows=1)
 data = np.loadtxt('tiemposCorregidos.txt', skiprows=1)
 
-"""
+
+""""
+# Test de permutacio'n para la media de los tiempos entre 
+# di'as soleados y lluviosos
+
+data0 = np.loadtxt('tiempos.txt', skiprows=1)
+data = np.loadtxt('tiemposCorregidos.txt', skiprows=1)
+
+tiempos_sol = data[:,1]
+tiempos_llu = data[:,3]
+tiempos = tiempos_sol + tiempos_llu
+
+delta0 = np.mean(tiempos_sol) - np.mean(tiempos_llu)
+
+l1 = [0] * len(tiempos_sol)
+l2 = [1] * len(tiempos_llu)
+labels = l1 + l2
+
+times = 1000
+deltas = []
+for i in range(0, times):
+    random.shuffle(labels)
+    tsol = 0
+    tllu = 0
+    for tiempo in zip(tiempos, labels):
+        if tiempo[1] == 0:
+            tsol += tiempo[0]
+        elif tiempo[1] == 1:
+            tllu += tiempo[0]
+
+    deltas.insert(0, tsol/len(tiempos_sol) - tllu/len(tiempos_llu))
+
+plt.hist(deltas, facecolor='g', alpha=0.75)
+plt.grid(True)
+plt.axvline(delta0, color='r')
+plt.xlabel('Deltas')
+plt.show()
+
+""""
+
+""""
 plt.figure()
 plt.scatter(data0[:,0], data0[:,1],label = "Soleado")
 plt.scatter(data0[:,0], data0[:,2],label = "Nublado")
@@ -42,7 +82,7 @@ n_tiempo_lluvia=stats.shapiro(data[:,3])
 print("normalidad tiempo_sol:", n_tiempo_sol)
 print("normalidad tiempo_nublado:", n_tiempo_nublado)
 print("normalidad tiempo_lluvia:", n_tiempo_lluvia)
-"""
+
 
 w_sol_lluvia= stats.wilcoxon(data[:,1], data[:,3])
 w_nublado_lluvia= stats.wilcoxon(data[:,2], data[:,3])
@@ -50,6 +90,7 @@ w_sol_nublado= stats.wilcoxon(data[:,1], data[:,2])
 print("wilcoxon_tiempo_sol tiempo_lluvia:", w_sol_lluvia)
 print("wilcoxon_tiempo_nublado tiempo_lluvia:", w_nublado_lluvia)
 print("wilcoxon_tiempo_sol tiempo_nublado:", w_sol_nublado)
+"""
 
 """
 pearson_sol_nublado=stats.pearsonr(data[:,1], data[:,2])
