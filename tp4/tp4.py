4ead43519fabec2b56784a334353dbf726f2358c
nico <nico@localhost.localdomain>
2017-10-17 21:05:16 -0300

    Mayoria de los 3 primeros puntos

--- tp4/tp4.py
+++ tp4/tp4.py
@@ -22,19 +22,51 @@ def add_paper(G, linea):
             G.add_edges_from(edges)
         e = e + 1
 
-
+# Punto 1: Creacion del grafo
 with open('data.csv', 'r') as data:
     papers = csv.reader(data)
     G = nx.Graph()
-    for paper in papers:      
+    for paper in papers:
         add_paper(G, paper)
-    
-    print("Cantidad de nodos: " + str(len(list(G.nodes()))))
-    print("Cantidad de aristas: " + str(len(list(G.edges()))))
 
-    # Histograma de grados
-    print(nx.degree(G))
-    plt.hist(sorted([e[1] for e in list(nx.degree(G)) ], reverse=True))
-    plt.show()
+cant_nodos = len(list(G.nodes()))
+cant_aristas = len(list(G.edges()))
+print("Cantidad de nodos: " + str(cant_nodos))
+print("Cantidad de aristas: " + str(cant_aristas))
+
+# Punto 2: Distribucion de grados
+dg = sorted([e[1] for e in list(nx.degree(G))], reverse=True)  
+
+plt.plot(dg)
+plt.title('Distribución de grado (lineal)')
+plt.xlabel('Nodo')
+plt.ylabel('Grado')
+plt.savefig('graficos/dg_lineal.jpg', dpi=300)
+plt.close()
+
+plt.loglog(dg)
+plt.title('Distribución de grado (loglog)')
+plt.xlabel('Nodo')
+plt.ylabel('Grado')
+plt.savefig('graficos/dg_loglog.jpg', dpi=300)
+plt.close()
+
+plt.semilogy(dg)
+plt.title('Distribución de grado (semilog y)')
+plt.xlabel('Nodo')
+plt.ylabel('Grado')
+plt.savefig('graficos/dg_semilog.jpg', dpi=300)
+plt.close()
+
+# Punto 3 : Componentes Conexas
+ncc = nx.number_connected_components(G)
+print("Numero de componentes conexas: " + str(ncc))
     
+    # cg es la componente gigante
+cg = max(nx.connected_component_subgraphs(G), key=len)
+tamcg = len(list(cg.nodes()))
+print("Tamaño de la componente gigante: " + str(tamcg))
+
+
+
 
